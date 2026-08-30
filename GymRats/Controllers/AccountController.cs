fcd4d44be846befbf63ef00a0a6dbb68257a5117
TaherAbubaker <t.abubaker4@student.aaup.edu>
2026-08-30 16:46:55 +0300

    Register Flow

---
+++ GymRats/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using GymRats.Data;
+using GymRats.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymRats.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ApplicationDbContext context { get; set; }
+
+        public AccountController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
