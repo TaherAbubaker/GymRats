0d7e00ca79b263333eef7a1976f92f5e48caeea0
TaherAbubaker <t.abubaker4@student.aaup.edu>
2026-08-31 17:01:45 +0300

    added an admin account to be used in production

---
+++ GymRats/Migrations/20260831135527_UpdateSchema.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymRats.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "IsAdmin", "Name", "PasswordHash" },
+                values: new object[] { 1, "admin@gmail.com", true, "Admin", "admin123" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
